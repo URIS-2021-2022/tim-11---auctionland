80b6c2b6c3293019a4ab2159d7944f1bd047dc59
Aleksandar Apatovic <alekap95@hotmail.com>
2022-02-01 23:39:04 +0100

    Db connection and controler added to parcel service

---
+++ ParcelService/ParcelService/Database/Parcel.cs
@@ -0,0 +1,24 @@
+﻿namespace ParcelService.Database
+{
+    public class Parcel
+    {
+        public int BrojPacele { get; set; }
+        public int KorisnikParcele { get; set; }
+        public int KatastarskaOpstina { get; set; }
+        public int Povrsina { get; set; }
+        public int BrListaNepokretnosti { get; set; }
+        public string Kultura { get; set; }
+        public string Klasa { get; set; }
+        public string Obradivost { get; set; }
+        public string ZasticenaZona { get; set; }
+        public string OblikSvojine { get; set; }
+        public string Odvodnjavanje { get; set; }
+        public string KulturaStvStanje { get; set; }
+        public string KlasaStvStanje { get; set; }
+        public string ObradivostStvStanje { get; set; }
+        public string ZasticenaZonaStvStanje { get; set; }
+        public string OblikSvojineStvStanje { get; set; }
+
+
+    }
+}
---
+++ ParcelService/ParcelService/Database/ParcelDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ParcelService.Database
+{
+    public class ParcelDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=THUNDER-PC\SQLEXPRESS;Initial Catalog=ParcelServiceDb; User ID = ParcelService; Password = parcel123; ");
+        }
+    }
+}
